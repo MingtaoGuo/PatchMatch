db9e46a40c75b756e0ce3dd897694f0dbca23cf6
unknown <gaspard.dussert@gmail.com>
2021-01-26 20:20:45 +0100

    fix propagation and speed up using numba

--- PatchMatch.py
+++ PatchMatch.py
@@ -1,14 +1,25 @@
 import numpy as np
 from PIL import Image
 import time
+from numba import jit
+import matplotlib.pyplot as plt
 
+#cette fonction est tres souvent appelée
+@jit(nopython=True)
 def cal_distance(a, b, A_padding, B, p_size):
     p = p_size // 2
     patch_a = A_padding[a[0]:a[0]+p_size, a[1]:a[1]+p_size, :]
     patch_b = B[b[0]-p:b[0]+p+1, b[1]-p:b[1]+p+1, :]
     temp = patch_b - patch_a
-    num = np.sum(1 - np.int32(np.isnan(temp)))
-    dist = np.sum(np.square(np.nan_to_num(temp))) / num
+    num = 0
+    dist = 0
+    for i in range(p_size):
+        for j in range(p_size):
+            for k in range(3):
+                if not np.isnan(temp[i,j,k]):
+                    num +=1 
+                    dist += np.square(temp[i,j,k])
+    dist /= num
     return dist
 
 def reconstruction(f, A, B):
@@ -18,8 +29,7 @@ def reconstruction(f, A, B):
     for i in range(A_h):
         for j in range(A_w):
             temp[i, j, :] = B[f[i, j][0], f[i, j][1], :]
-    Image.fromarray(temp).show()
-
+    Image.fromarray(temp).save("reconstruction.png")
 
 def initialization(A, B, p_size):
     A_h = np.size(A, 0)
@@ -41,33 +51,21 @@ def initialization(A, B, p_size):
             dist[i, j] = cal_distance(a, b, A_padding, B, p_size)
     return f, dist, A_padding
 
-def propagation(f, a, dist, A_padding, B, p_size, is_odd):
-    A_h = np.size(A_padding, 0) - p_size + 1
-    A_w = np.size(A_padding, 1) - p_size + 1
-    x = a[0]
-    y = a[1]
-    if is_odd:
-        d_left = dist[max(x-1, 0), y]
-        d_up = dist[x, max(y-1, 0)]
-        d_current = dist[x, y]
-        idx = np.argmin(np.array([d_current, d_left, d_up]))
-        if idx == 1:
-            f[x, y] = f[max(x - 1, 0), y]
-            dist[x, y] = cal_distance(a, f[x, y], A_padding, B, p_size)
-        if idx == 2:
-            f[x, y] = f[x, max(y - 1, 0)]
-            dist[x, y] = cal_distance(a, f[x, y], A_padding, B, p_size)
-    else:
-        d_right = dist[min(x + 1, A_h-1), y]
-        d_down = dist[x, min(y + 1, A_w-1)]
-        d_current = dist[x, y]
-        idx = np.argmin(np.array([d_current, d_right, d_down]))
-        if idx == 1:
-            f[x, y] = f[min(x + 1, A_h-1), y]
-            dist[x, y] = cal_distance(a, f[x, y], A_padding, B, p_size)
-        if idx == 2:
-            f[x, y] = f[x, min(y + 1, A_w-1)]
-            dist[x, y] = cal_distance(a, f[x, y], A_padding, B, p_size)
+# je fais cette fonction car numba arrive pas 
+# a trouver le type de f dans random_search
+@jit(nopython=True)
+def inter(b_x, search_h,p,B_h,b_y, search_w,B_w,alpha,i): 
+    search_min_r = max(b_x - search_h, p)
+    search_max_r = min(b_x + search_h, B_h-p)
+    random_b_x = np.random.randint(search_min_r, search_max_r)
+    search_min_c = max(b_y - search_w, p)
+    search_max_c = min(b_y + search_w, B_w - p)
+    random_b_y = np.random.randint(search_min_c, search_max_c)
+    search_h = B_h * alpha ** i
+    search_w = B_w * alpha ** i
+    b = np.array([random_b_x, random_b_y])
+    return search_h, search_w, b
+    
 
 def random_search(f, a, dist, A_padding, B, p_size, alpha=0.5):
     x = a[0]
@@ -80,27 +78,52 @@ def random_search(f, a, dist, A_padding, B, p_size, alpha=0.5):
     search_w = B_w * alpha ** i
     b_x = f[x, y][0]
     b_y = f[x, y][1]
+    alpha=alpha
     while search_h > 1 and search_w > 1:
-        search_min_r = max(b_x - search_h, p)
-        search_max_r = min(b_x + search_h, B_h-p)
-        random_b_x = np.random.randint(search_min_r, search_max_r)
-        search_min_c = max(b_y - search_w, p)
-        search_max_c = min(b_y + search_w, B_w - p)
-        random_b_y = np.random.randint(search_min_c, search_max_c)
-        search_h = B_h * alpha ** i
-        search_w = B_w * alpha ** i
-        b = np.array([random_b_x, random_b_y])
+        search_h, search_w, b = inter(b_x, search_h,p,B_h,b_y, search_w,B_w,alpha,i)
         d = cal_distance(a, b, A_padding, B, p_size)
         if d < dist[x, y]:
             dist[x, y] = d
             f[x, y] = b
         i += 1
 
+def propagation(f, a, dist, A_padding, B, p_size, is_odd):
+    A_h = np.size(A_padding, 0) - p_size + 1
+    A_w = np.size(A_padding, 1) - p_size + 1
+    x = a[0]
+    y = a[1]
+    if is_odd:
+        d_left = cal_distance(a, f[max(x - 1, 0), y], A_padding, B, p_size)
+        d_up = cal_distance(a, f[x, max(y - 1, 0)], A_padding, B, p_size)
+        d_current = dist[x, y]
+        idx = np.argmin(np.array([d_current, d_left, d_up]))
+        if idx == 1:
+            f[x, y] = f[max(x - 1, 0), y]
+            dist[x, y] = d_left
+        if idx == 2:
+            f[x, y] = f[x, max(y - 1, 0)]
+            dist[x, y] = d_up
+    else:
+        d_right = cal_distance(a, f[min(x + 1, A_h-1), y], A_padding, B, p_size)
+        d_down = cal_distance(a, f[x, min(y + 1, A_w-1)], A_padding, B, p_size)
+        d_current = dist[x, y]
+        idx = np.argmin(np.array([d_current, d_right, d_down]))
+        if idx == 1:
+            f[x, y] = f[min(x + 1, A_h-1), y]
+            dist[x, y] = d_right
+        if idx == 2:
+            f[x, y] = f[x, min(y + 1, A_w-1)]
+            dist[x, y] = d_down
+
 def NNS(img, ref, p_size, itr):
     A_h = np.size(img, 0)
     A_w = np.size(img, 1)
+    print("initialization")
     f, dist, img_padding = initialization(img, ref, p_size)
+    print(f"score : {dist.mean()}")
+    score_list = []
     for itr in range(1, itr+1):
+        print("iteration: %d"%(itr))
         if itr % 2 == 0:
             for i in range(A_h - 1, -1, -1):
                 for j in range(A_w - 1, -1, -1):
@@ -113,8 +136,10 @@ def NNS(img, ref, p_size, itr):
                     a = np.array([i, j])
                     propagation(f, a, dist, img_padding, ref, p_size, True)
                     random_search(f, a, dist, img_padding, ref, p_size)
-        print("iteration: %d"%(itr))
-    return f
+        print(f"score : {dist.mean()}")
+        score_list.append(dist.mean())
+    return f, dist, score_list
+
 
 if __name__ == "__main__":
     img = np.array(Image.open("./cup_a.jpg"))
@@ -122,7 +147,8 @@ if __name__ == "__main__":
     p_size = 3
     itr = 5
     start = time.time()
-    f = NNS(img, ref, p_size, itr)
+    f, dist, score_list = NNS(img, ref, p_size, itr)
     end = time.time()
-    print(end - start)
+    print(f"Time :  {end - start}")
     reconstruction(f, img, ref)
+    plt.plot(score_list)
